fc003cee5664a108e11ffe496bfb3d74c2267e4a
김상민 <smkim5698@gmail.com>
2024-05-13 09:22:44 +0900

    240513-0922 : s03 : 준영속 상태

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -10,7 +10,8 @@ public class Member {
     private Long id;
     private String name;
 
-    public Member() {}
+    public Member() {
+    }
 
     public Member(Long id, String name) {
         this.id = id;
