4947617ca6ee127d41fb492d758c19819f4b8ebe
김상민 <smkim5698@gmail.com>
2024-05-13 12:43:18 +0900

    240513-1244 : s04 : 필드와 컬럼 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -5,9 +5,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class JpaMain {
 
@@ -16,18 +14,7 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
-            em.persist(member);
-
-            Member findMember = em.find(Member.class, 100L);
-            System.out.println("-> findMember.id : " + findMember.getId());
-            System.out.println("-> findMember.name : " + findMember.getName());
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
