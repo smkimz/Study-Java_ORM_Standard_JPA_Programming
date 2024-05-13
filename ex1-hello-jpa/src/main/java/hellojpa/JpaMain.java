a6566b9598139bf0fd19c71f75ba5b65a3fe550f
김상민 <smkim5698@gmail.com>
2024-05-13 15:06:27 +0900

    240513-1506 : s04 : 실전 예제 1 - 요구사항 분석과 기본 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,6 +15,24 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Member member1 = new Member();
+            member1.setUsername("A");
+
+            Member member2 = new Member();
+            member2.setUsername("B");
+
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            System.out.println("=========================");
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+            System.out.println("-> member1.id : " + member1.getId());   // DB SEQ = 1, Application = 1
+            System.out.println("-> member2.id : " + member2.getId());   // DB SEQ = 51, Application = 2 (메모리)
+            System.out.println("-> member3.id : " + member3.getId());   // DB SEQ = 51, Application = 3 (메모리)
+            System.out.println("=========================");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -5,9 +5,15 @@ import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1, allocationSize = 50
+)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name", nullable = false, unique = true)
@@ -30,4 +36,19 @@ public class Member {
     public Member() {
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
