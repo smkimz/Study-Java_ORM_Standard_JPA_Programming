fc0e4975852ca02e5d7159bec87527442ceff572
김상민 <smkim5698@gmail.com>
2024-05-13 09:41:34 +0900

    240513-0941 : s04 : 객체와 테이블 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,104 +18,22 @@ public class JpaMain {
         tx.begin();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("-> Input your order : ");
-        String order = br.readLine();
-        if (order.equals("1")) {
-            try {
-                // 비영속
-                Member member = new Member();
-                member.setId(100L);
-                member.setName("HelloJPA");
+        try {
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+            em.persist(member);
 
-                // 영속
-                System.out.println("-> BEFORE");
-                em.persist(member);
-                System.out.println("-> AFTER");
+            Member findMember = em.find(Member.class, 100L);
+            System.out.println("-> findMember.id : " + findMember.getId());
+            System.out.println("-> findMember.name : " + findMember.getName());
 
-                Member findMember = em.find(Member.class, 100L);
-                System.out.println("-> findMember.id : " + findMember.getId());
-                System.out.println("-> findMember.name : " + findMember.getName());
-
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-            } finally {
-                em.close();
-            }
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
         }
-
-        if (order.equals("2")) {
-            try {
-                Member member1 = new Member(101L, "A");
-                Member member2 = new Member(102L, "B");
-                em.persist(member1);
-                em.persist(member2);
-                System.out.println("====================");
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-            } finally {
-                em.close();
-            }
-        }
-
-        if (order.equals("3")) {
-            try {
-                Member member = em.find(Member.class, 100L);
-                member.setName("ZZZZZ");
-                // em.persist(member)가 없어도 자동으로 member가 업데이트 된다.
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-            } finally {
-                em.close();
-            }
-        }
-
-        if (order.equals("4")) {
-            try {
-                Member member = new Member(100L, "Member");
-                em.persist(member);
-                em.flush();
-                System.out.println("=========================");
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-            } finally {
-                em.close();
-            }
-        }
-
-        if (order.equals("5")) {
-            try {
-                Member member = em.find(Member.class, 100L);
-                member.setName("AAAAA");
-                em.detach(member);
-                System.out.println("=========================");
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-            } finally {
-                em.close();
-            }
-        }
-
-        if (order.equals("6")) {
-            try {
-                Member member1 = em.find(Member.class, 100L);
-                member1.setName("BBBBB");
-                em.clear();
-                Member member2 = em.find(Member.class, 100L);
-                System.out.println("=========================");
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-            } finally {
-                em.close();
-            }
-        }
-
-
         emf.close();
     }
 }
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,8 +2,10 @@ package hellojpa;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "TBE")
 public class Member {
 
     @Id
