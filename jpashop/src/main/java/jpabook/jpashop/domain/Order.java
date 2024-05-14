3b4e637d051cabd048bb65492dbbbd978b3b97d1
김상민 <smkim5698@gmail.com>
2024-05-14 16:44:42 +0900

    240514-1645 : s05 : 실전 예제 2 - 연관관계 매핑 시작

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -3,53 +3,64 @@ package jpabook.jpashop.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
 public class Order {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "ORDER_ID")
-    private Long id;
+	@Id
+	@GeneratedValue
+	@Column(name = "ORDER_ID")
+	private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+	@ManyToOne
+	@JoinColumn(name = "MEMBER_ID")
+	private Member member;
 
-    private LocalDateTime orderDate;
+	@OneToMany(mappedBy = "order")
+	private List<OrderItem> orderItems = new ArrayList<>();
 
-    @Enumerated(EnumType.STRING)
-    private OrderStatus status;
+	private LocalDateTime orderDate;
 
-    public Long getId() {
-        return id;
-    }
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void addOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
 
-    public Long getMemberId() {
-        return memberId;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
+	public Member getMember() {
+		return member;
+	}
 
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
+	public void setMember(Member member) {
+		this.member = member;
+	}
 
-    public OrderStatus getStatus() {
-        return status;
-    }
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
 
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
 }
