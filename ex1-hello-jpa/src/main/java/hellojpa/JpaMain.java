153d33f4cbf18847e12239f84fb307001a5f2b16
김상민 <smkim5698@gmail.com>
2024-05-14 15:54:54 +0900

    240514-1555 : s05 : 양방향 연관관계와 연관관계의 주인 2 - 주의점, 정리

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -21,31 +21,19 @@ public class JpaMain {
 
 			Member member = new Member();
 			member.setUsername("member1");
-			member.setTeam(team);
 			em.persist(member);
+			team.addMember(member);
 
 			em.flush();
 			em.clear();
 
-			Member findMember = em.find(Member.class, member.getId());
-			List<Member> members = findMember.getTeam().getMembers();
-
-            /*
-                Member findMember = em.find(Member.class, member.getId());를 실행하면
-                영속성 컨텍스트에서 member 엔티티를 찾을 수 있지만,
-                List<Member> members = findMember.getTeam().getMembers();를 실행할 때는
-                team 객체와 연결된 member 객체를 데이터베이스에서 찾아야 하는데,
-                아직 member 엔티티가 데이터베이스에 반영되지 않았기 때문에
-                member 객체를 찾을 수 없어서 출력이 되지 않는 것입니다.
-
-                따라서 em.flush();를 호출하여 영속성 컨텍스트의 상태를 데이터베이스에 동기화하고,
-                em.clear();를 호출하여 영속성 컨텍스트를 초기화한 후 다시 데이터베이스에서 member 엔티티를 조회하면,
-                team 객체와 연결된 member 객체를 정상적으로 찾아 출력할 수 있습니다.
-            */
-
+			Team findTeam = em.find(Team.class, team.getId()); // 1차 캐시
+			List<Member> members = findTeam.getMembers();
+			System.out.println("==============================");
 			for (Member m : members) {
 				System.out.println("-> member : " + m.getUsername());
 			}
+			System.out.println("==============================");
 
 			tx.commit();
 		} catch (Exception e) {
