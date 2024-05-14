2ae17477f45ee2c95d43b0ffd1e4d9b24299f58b
김상민 <smkim5698@gmail.com>
2024-05-14 17:48:20 +0900

    240514-1748 : s06 : 일대다 [1:N]

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -14,7 +14,7 @@ public class Member {
 	private String username;
 
 	@ManyToOne
-	@JoinColumn(name = "TEAM_ID")
+	@JoinColumn(name = "TEAM_ID", insertable = false, updatable = false) //**
 	private Team team;
 
 	public Long getId() {
@@ -33,11 +33,4 @@ public class Member {
 		this.username = username;
 	}
 
-	public Team getTeam() {
-		return team;
-	}
-
-	public void setTeam(Team team) {
-		this.team = team;
-	}
 }
