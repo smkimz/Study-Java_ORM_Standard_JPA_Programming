92e19e8716c93ece64e5743b29fb54c96623c9f6
김상민 <smkim5698@gmail.com>
2024-05-22 17:37:35 +0900

    240522-1738 : s08 : 즉시 로딩과 지연 로딩

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import org.hibernate.Hibernate;
+
+import java.util.List;
 
 public class JpaMain {
 
@@ -14,15 +15,29 @@ public class JpaMain {
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 		try {
-			Member member = new Member();
-			member.setUsername("AAA");
-			em.persist(member);
+			Team teamA = new Team();
+			teamA.setName("TeamA");
+			em.persist(teamA);
+
+			Team teamB = new Team();
+			teamB.setName("TeamB");
+			em.persist(teamB);
+
+			Member memberA = new Member();
+			memberA.setUsername("MemberA");
+			memberA.setTeam(teamA);
+			em.persist(memberA);
+
+			Member memberB = new Member();
+			memberB.setUsername("MemberB");
+			memberB.setTeam(teamB);
+			em.persist(memberB);
+
 			em.flush();
 			em.clear();
 
-			Member reference = em.getReference(Member.class, member.getId());
-			System.out.println("member(reference) : " + reference.getClass());
-			Hibernate.initialize(reference);
+			List<Member> members = em.createQuery("SELECT m FROM Member m JOIN FETCH m.team", Member.class).getResultList();
+
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
